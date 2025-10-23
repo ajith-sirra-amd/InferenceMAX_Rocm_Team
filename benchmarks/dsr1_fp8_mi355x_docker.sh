88bc243e4b391d9e53ac60da7671b0ad6bb236db
seungrokjung <seungrok.jung@amd.com>
2025-10-22 22:59:50 -0500

    workflow update
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- benchmarks/dsr1_fp8_mi355x_docker.sh
+++ benchmarks/dsr1_fp8_mi355x_docker.sh
@@ -8,18 +8,17 @@
 # TP
 # CONC
 # MAX_MODEL_LEN
-max_model_len=16384            # Must be >= the input + output length
-max_seq_len_to_capture=10240   # Beneficial to set this to max_model_len
-max_num_seqs=1024
+
+# Reference
+# https://amd.atlassian.net/wiki/spaces/RPLBAS/pages/1149960861/WIP+rocm+7.0+rocm7.0_ubuntu_22.04_vllm_0.10.1_instinct_20250927_rc1
+
+max_model_len=$MAX_MODEL_LEN            # Must be >= the input + output length
+max_seq_len_to_capture=$MAX_MODEL_LEN   # Beneficial to set this to max_model_len
+max_num_seqs=$CONC
 max_num_batched_tokens=131072  # Smaller values may result in better TTFT but worse TPOT / Throughput
-
-export VLLM_USE_V1=1
-export VLLM_USE_AITER_TRITON_ROPE=1
-export VLLM_ROCM_USE_AITER=1
-export VLLM_ROCM_USE_AITER_RMSNORM=1
-export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
-
-
+# Note: this flag may not be compatible with MI325X
+#export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
+# Note: Using `--kv-cache-dtype fp8` with DeepSeek may cause accuracy issues
 
 vllm serve ${MODEL} \
     --host localhost \
