08d8c4753e6622bbab37999e4954d471b9e4479a
seungrokjung <seungrok.jung@amd.com>
2025-10-30 21:11:10 -0500

    num prompts fix
    Signed-off-by: seungrokjung <seungrok.jung@amd.com>

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -101,7 +101,7 @@ if [[ "$run_benchmark" != "false" ]]; then
   --model=$MODEL --backend=vllm --base-url="http://$server_name:$PORT" \
   --dataset-name=random \
   --random-input-len=$ISL --random-output-len=$OSL --random-range-ratio=$RANDOM_RANGE_RATIO \
-  --num-prompts=$(( $CONC * 10 )) \
+  --num-prompts=$NUM_PROMPTS \
   --max-concurrency=$CONC \
   --request-rate=inf --ignore-eos \
   --save-result --percentile-metrics="ttft,tpot,itl,e2el" \
