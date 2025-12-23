8bda91ed607ed3691961d2c32f005e66d033bfbc
Shekhar <38083203+indianspeedster@users.noreply.github.com>
2025-12-22 16:32:40 -0800

    Update dsr1_fp8_mi355x_docker.sh

--- benchmarks/dsr1_fp8_mi355x_docker.sh
+++ benchmarks/dsr1_fp8_mi355x_docker.sh
@@ -30,6 +30,6 @@ vllm serve ${MODEL} \
     --max-model-len ${max_model_len} \
     --block-size 1 \
     --gpu-memory-utilization 0.95 \
-    --max-seq-len-to-capture ${max_seq_len_to_capture} \
+   # --max-seq-len-to-capture ${max_seq_len_to_capture} \
     --async-scheduling \
     --kv-cache-dtype auto
