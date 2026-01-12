44a64f00c0fce39c8f56f4bcaa0ebdba59b16d98
Rohan138 <rohanpotdar138@gmail.com>
2026-01-12 15:18:36 -0600

    Run gpt-oss w4a8 on amd_dev 0103

--- benchmarks/gptoss_fp4_mi355x_docker.sh
+++ benchmarks/gptoss_fp4_mi355x_docker.sh
@@ -12,14 +12,13 @@
 export VLLM_USE_AITER_UNIFIED_ATTENTION=1 VLLM_ROCM_USE_AITER=1 VLLM_ROCM_USE_AITER_MHA=0
 export VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION=1
 export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
-export VLLM_ROCM_USE_AITER_FUSED_MOE_A16W4=1
+# export VLLM_ROCM_USE_AITER_FUSED_MOE_A16W4=1
 
 set -x
 vllm serve $MODEL --port $PORT \
     --tensor-parallel-size=$TP \
     --gpu-memory-utilization 0.95 \
     --max-model-len $MAX_MODEL_LEN \
-    --max-seq-len-to-capture $MAX_MODEL_LEN \
     --compilation-config '{"cudagraph_mode":"FULL_AND_PIECEWISE"}' \
     --block-size=64 \
     --no-enable-prefix-caching \
