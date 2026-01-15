1e8203c069b2b28f97efa1a5869ce91a551b0f32
Rohan138 <rohanpotdar138@gmail.com>
2026-01-15 01:22:10 -0600

    update to 0113 upstream main vllm image

--- benchmarks/gptoss_fp4_mi325x_docker.sh
+++ benchmarks/gptoss_fp4_mi325x_docker.sh
@@ -24,9 +24,11 @@ if [[ "$version" == "" || $version -lt 177 ]]; then
   export HSA_NO_SCRATCH_RECLAIM=1
 fi
 
-export VLLM_USE_AITER_UNIFIED_ATTENTION=1
+export VLLM_ROCM_USE_AITER=1
 export VLLM_ROCM_USE_AITER_MHA=0
-export VLLM_ROCM_USE_AITER_TRITON_BF16_GEMM=0
+export VLLM_ROCM_USE_AITER_UNIFIED_ATTENTION=1
+export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
+export VLLM_ROCM_USE_AITER_TRITON_GEMM=0
 
 SERVER_LOG=$(mktemp /tmp/server-XXXXXX.log)
 
@@ -35,7 +37,6 @@ vllm serve $MODEL --port $PORT \
 --tensor-parallel-size=$TP \
 --gpu-memory-utilization 0.95 \
 --max-model-len $MAX_MODEL_LEN \
---max-seq-len-to-capture $MAX_MODEL_LEN \
 --compilation-config  '{"cudagraph_mode": "FULL_AND_PIECEWISE"}' \
 --block-size=64 \
 --no-enable-prefix-caching \
