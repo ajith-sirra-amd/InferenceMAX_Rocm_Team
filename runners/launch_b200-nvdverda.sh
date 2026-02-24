d4ee5ede7283878820e3c40ebebfb5e4cc505259
seungrokj <144636725+seungrokj@users.noreply.github.com>
2026-02-24 11:27:59 +0900

    Update launch_b200-nvdverda.sh

--- runners/launch_b200-nvdverda.sh
+++ runners/launch_b200-nvdverda.sh
@@ -62,8 +62,9 @@ NGINX_SQUASH_FILE="/home/ubuntu/InferenceMAX_rocm/containers/$(echo "$NGINX_IMAG
 
 #srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
 #srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c "enroot import -o $NGINX_SQUASH_FILE docker://$NGINX_IMAGE"
-srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c $SQUASH_FILE
-srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c $NGINX_SQUASH_FILE 
+
+#srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c $SQUASH_FILE
+#srun -N 1 -A $SLURM_ACCOUNT -p $SLURM_PARTITION bash -c $NGINX_SQUASH_FILE 
 
 # Create srtslurm.yaml for srtctl
 echo "Creating srtslurm.yaml configuration..."
