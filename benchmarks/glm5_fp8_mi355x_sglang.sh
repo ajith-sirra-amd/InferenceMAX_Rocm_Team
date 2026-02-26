e0d0dd75c4406774baf317f81207bfacab6f5893
ajith-sirra-amd <ajith.sirra@amd.com>
2026-02-26 16:43:55 +0530

    Modified GLM5 SGLANG SERVER Command

--- benchmarks/glm5_fp8_mi355x_sglang.sh
+++ benchmarks/glm5_fp8_mi355x_sglang.sh
@@ -36,7 +36,8 @@ python3 -m sglang.launch_server \
     --tensor-parallel-size $TP \
     --trust-remote-code \
     --mem-fraction-static $MEM_FRAC_STATIC \
-    --kv-cache-dtype fp8_e4m3 > $SERVER_LOG 2>&1 &
+    --nsa-prefill-backend tilelang \
+    --nsa-decode-backend tilelang > $SERVER_LOG 2>&1 &
 
 SERVER_PID=$!
 
