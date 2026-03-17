68712d7d894f12f88c38118e1d38894c15b2cb46
seungrokj <seungrok.jung@amd.com>
2026-03-17 01:12:20 +0000

    slurm
    Signed-off-by: seungrokj <seungrok.jung@amd.com>

--- runners/launch_mi355x-amds.sh
+++ runners/launch_mi355x-amds.sh
@@ -158,7 +158,7 @@ else
 
     PARTITION="amd-tw"
     SQUASH_FILE="/var/lib/squash/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
-    LOCK_FILE="${SQUASH_FILE}.lock"
+    #LOCK_FILE="${SQUASH_FILE}.lock"
 
     set -x
     salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell --job-name="$RUNNER_NAME"
@@ -166,17 +166,27 @@ else
 
     srun --jobid=$JOB_ID bash -c "docker stop \$(docker ps -a -q)"
 
-    # Use flock to serialize concurrent imports to the same squash file
-    srun --jobid=$JOB_ID bash -c "
-        exec 9>\"$LOCK_FILE\"
-        flock -w 600 9 || { echo 'Failed to acquire lock for $SQUASH_FILE'; exit 1; }
-        if unsquashfs -l \"$SQUASH_FILE\" > /dev/null 2>&1; then
-            echo 'Squash file already exists and is valid, skipping import'
-        else
-            rm -f \"$SQUASH_FILE\"
-            enroot import -o \"$SQUASH_FILE\" docker://$IMAGE
-        fi
-    "
+    ## Use flock to serialize concurrent imports to the same squash file
+    #srun --jobid=$JOB_ID bash -c "
+    #    exec 9>\"$LOCK_FILE\"
+    #    flock -w 600 9 || { echo 'Failed to acquire lock for $SQUASH_FILE'; exit 1; }
+    #    if [[ \"$FRAMEWORK\" == \"atom\" ]]; then
+    #        rm -f \"$SQUASH_FILE\"
+    #    fi
+    #    if unsquashfs -l \"$SQUASH_FILE\" > /dev/null 2>&1; then
+    #        echo 'Squash file already exists and is valid, skipping import'
+    #    else
+    #        rm -f \"$SQUASH_FILE\"
+    #        enroot import -o \"$SQUASH_FILE\" docker://$IMAGE
+    #    fi
+    #"
+
+    srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+    if ! srun --jobid=$JOB_ID bash -c "unsquashfs -l $SQUASH_FILE > /dev/null"; then
+        echo "unsquashfs failed, removing $SQUASH_FILE and re-importing..."
+        srun --jobid=$JOB_ID bash -c "rm -f $SQUASH_FILE"
+        srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+    fi
 
     export VLLM_CACHE_ROOT="/it-share/gharunners/.cache/vllm"
 
