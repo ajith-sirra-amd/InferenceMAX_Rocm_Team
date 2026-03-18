813df365cf2ec7653c5af2eb02992f56889d6d0d
seungrok.jung <seungrok.jung@amd.com>
2026-03-18 06:37:49 -0500

    update
    Signed-off-by: seungrok.jung <seungrok.jung@amd.com>

--- benchmarks/kimik2.5_int4_mi355x_vllm.sh
+++ benchmarks/kimik2.5_int4_mi355x_vllm.sh
@@ -36,7 +36,6 @@ vllm serve $MODEL --port $PORT \
 --tensor-parallel-size=$TP \
 --gpu-memory-utilization 0.95 \
 --max-num-seqs 256 \
---disable-log-requests \
 --trust-remote-code \
 --block-size=64 \
 --max-num-seqs 256 \
