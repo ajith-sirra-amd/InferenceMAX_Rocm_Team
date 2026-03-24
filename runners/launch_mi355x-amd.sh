01f169b0a0cd38d9f14058789c6fcce0522e9419
ajith-sirra-amd <122240613+ajith-sirra-amd@users.noreply.github.com>
2026-03-24 09:33:35 +0530

    Update launch_mi355x-amd.sh

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -29,7 +29,7 @@ else
 fi
 
 set -x
-podman run --rm --init --network host --shm-size=16g --name=$server_name \
+podman run --rm --network host --shm-size=16g --name=$server_name \
 --privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
