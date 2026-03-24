37b3cbe0e0afc2fc9030bd9e35d8367b330a5f62
ajith-sirra-amd <122240613+ajith-sirra-amd@users.noreply.github.com>
2026-03-24 12:20:37 +0530

    Update launch_mi355x-amds.sh

--- runners/launch_mi355x-amds.sh
+++ runners/launch_mi355x-amds.sh
@@ -14,7 +14,7 @@ SPEC_SUFFIX=$([[ "$SPEC_DECODING" == "mtp" ]] && printf '_mtp' || printf '')
 server_name="bmk-server"
 
 # Cleanup: stop server container 
-# docker stop $server_name 2>/dev/null || true
+podman stop $server_name 2>/dev/null || true
 podman rm -f $server_name 2>/dev/null || true
 echo "launch_mi355x_amds.sh"
 
@@ -58,5 +58,5 @@ if ls gpucore.* 1> /dev/null 2>&1; then
 fi
 
 # Cleanup: stop server container 
-# docker stop $server_name 2>/dev/null || true
-# docker rm $server_name 2>/dev/null || true
+podman stop $server_name 2>/dev/null || true
+podman rm $server_name 2>/dev/null || true
