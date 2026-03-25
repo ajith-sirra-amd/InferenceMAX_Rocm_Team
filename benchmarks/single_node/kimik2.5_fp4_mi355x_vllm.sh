f74895fe8be71664917edf8051161dd261dae811
seungrokj <seungrok.jung@amd.com>
2026-03-25 07:54:37 -0500

    debug
    Signed-off-by: seungrokj <seungrok.jung@amd.com>

--- benchmarks/single_node/kimik2.5_fp4_mi355x_vllm.sh
+++ benchmarks/single_node/kimik2.5_fp4_mi355x_vllm.sh
@@ -41,8 +41,6 @@ if [[ "$version" == "" || $version -lt 177 ]]; then
   export HSA_NO_SCRATCH_RECLAIM=1
 fi
 
-export VLLM_ROCM_USE_AITER=1
-export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
 
 if [ "$EP_SIZE" -gt 1 ]; then
   EP=" --enable-expert-parallel"
@@ -57,6 +55,12 @@ fi
 start_gpu_monitor
 
 set -x
+export VLLM_ROCM_USE_AITER=1
+export VLLM_ROCM_QUICK_REDUCE_QUANTIZATION=INT4
+if [ "${TP}" = "4" ]; then
+  export VLLM_ROCM_USE_AITER_RMSNORM=0
+fi
+
 vllm serve $MODEL --port $PORT \
 --tensor-parallel-size=$TP \
 $EP \
@@ -66,15 +70,19 @@ $EP \
 --trust-remote-code \
 --no-enable-prefix-caching \
 --max-num-seqs 64 \
---max-num-batched-tokens 32768 \
 --mm-encoder-tp-mode data > $SERVER_LOG 2>&1 &
 
+#--max-num-batched-tokens 32768 \
 SERVER_PID=$!
 
 # Wait for server to be ready
 wait_for_server_ready --port "$PORT" --server-log "$SERVER_LOG" --server-pid "$SERVER_PID"
 
 export PYTHONDONTWRITEBYTECODE=1
+if [ "${RUN_EVAL}" = "true" ]; then
+ISL=10
+OSL=2
+fi
 run_benchmark_serving \
     --model "$MODEL" \
     --port "$PORT" \
