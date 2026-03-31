e81922460c7e14c395c38332b6fe990c13f8d876
seungrokj <seungrok.jung@amd.com>
2026-03-31 07:37:35 +0000

    update

--- benchmarks/single_node/dsr1_fp4_mi355x_atom_mtp.sh
+++ benchmarks/single_node/dsr1_fp4_mi355x_atom_mtp.sh
@@ -74,7 +74,7 @@ run_benchmark_serving \
 
 # After throughput, run evaluation only if RUN_EVAL is true
 if [ "${RUN_EVAL}" = "true" ]; then
-    run_eval --framework lm-eval --port "$PORT" --concurrent-requests $CONC
+    run_eval --framework lm-eval --port "$PORT" 
     append_lm_eval_summary
 fi
 
