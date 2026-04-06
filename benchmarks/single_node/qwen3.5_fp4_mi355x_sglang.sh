8e31f961b1137cf3ba45116a920dd0f1799827ef
ajith-sirra-amd <ajith.sirra@amd.com>
2026-04-06 12:12:55 +0530

    Accuracy Checks added by default - AMD MXFP4 - Optimizing Per by commenting mamba bf16

---
+++ benchmarks/single_node/qwen3.5_fp4_mi355x_sglang.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/../benchmark_lib.sh"
+
+check_env_vars \
+    MODEL \
+    TP \
+    CONC \
+    ISL \
+    OSL \
+    RANDOM_RANGE_RATIO \
+    RESULT_FILENAME
+
+if [[ -n "$SLURM_JOB_ID" ]]; then
+  echo "JOB $SLURM_JOB_ID running on $SLURMD_NODENAME"
+fi
+
+hf download "$MODEL"
+
+SERVER_LOG=/workspace/server.log
+PORT=${PORT:-8888}
+MEM_FRAC_STATIC=${MEM_FRAC_STATIC:-0.8}
+CHUNK_SIZE=8192
+
+# Start GPU monitoring (power, temperature, clocks every second)
+start_gpu_monitor
+
+set -x
+sglang serve \
+    --attention-backend triton \
+    --model-path $MODEL \
+    --host=0.0.0.0 \
+    --port $PORT \
+    --tensor-parallel-size $TP \
+    --trust-remote-code \
+    --mem-fraction-static $MEM_FRAC_STATIC \
+    --kv-cache-dtype fp8_e4m3 \
+    --cuda-graph-max-bs $CONC \
+    --max-running-requests $CONC \
+    --chunked-prefill-size $CHUNK_SIZE \
+    --max-prefill-tokens $CHUNK_SIZE \
+    --disable-radix-cache \
+    --num-continuous-decode-steps 2 \
+    > $SERVER_LOG 2>&1 &
+
+SERVER_PID=$!
+
+# Wait for server to be ready
+wait_for_server_ready --port "$PORT" --server-log "$SERVER_LOG" --server-pid "$SERVER_PID"
+export PYTHONDONTWRITEBYTECODE=1
+run_benchmark_serving \
+    --model "$MODEL" \
+    --port "$PORT" \
+    --backend vllm \
+    --input-len "$ISL" \
+    --output-len "$OSL" \
+    --random-range-ratio "$RANDOM_RANGE_RATIO" \
+    --num-prompts "$((CONC * 10))" \
+    --max-concurrency "$CONC" \
+    --result-filename "$RESULT_FILENAME" \
+    --result-dir /workspace/
+
+# After throughput, run evaluation only if RUN_EVAL is true
+# if [ "${RUN_EVAL}" = "true" ]; then
+run_eval --framework lm-eval --port "$PORT" --concurrent-requests $CONC
+append_lm_eval_summary
+# fi
+
+# Stop GPU monitoring
+stop_gpu_monitor
+set +x
