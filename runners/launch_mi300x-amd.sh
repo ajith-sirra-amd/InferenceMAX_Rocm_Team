b8a2e1913f7cce17cf6e4b009be71c8b085b7f6d
ajith-sirra-amd <ajith.sirra@amd.com>
2026-04-10 16:38:57 +0530

    Qwen3.5 MI350x Support

--- runners/launch_mi300x-amd.sh
+++ runners/launch_mi300x-amd.sh
@@ -1,21 +1,74 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+HF_HUB_CACHE_MOUNT="/mnt/hf_hub_cache/"  # Temp solution
 
-sudo sh -c 'echo 0 > /proc/sys/kernel/numa_balancing'
+MODEL_CODE="${EXP_NAME%%_*}"
+if [[ $FRAMEWORK == "vllm" ]]; then
+    FRAMEWORK_SUFFIX="_vllm"
+elif [[ $FRAMEWORK == "sglang" ]]; then
+    FRAMEWORK_SUFFIX="_sglang"
+elif [[ $FRAMEWORK == "atom" ]]; then
+    FRAMEWORK_SUFFIX="_atom"
+fi
+SPEC_SUFFIX=$([[ "$SPEC_DECODING" == "mtp" ]] && printf '_mtp' || printf '')
 
-HF_HUB_CACHE_MOUNT="/shareddata/hf_hub_cache_$(hostname)/"
-PORT=8888
+hostname
 
 server_name="bmk-server"
 
+# Cleanup: stop server container 
+docker stop $server_name 2>/dev/null || true
+docker rm $server_name 2>/dev/null || true
+
+original=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+
+echo $original
+
+# for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" | sudo tee $cpu; done
+
+# lscpu
+
 set -x
-docker run --rm --ipc=host --shm-size=16g --network=host --name=$server_name \
+docker pull $IMAGE
+DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$IMAGE" | cut -d'@' -f2)
+echo "The image digest is: $DIGEST"
+
+if [[ "$FRAMEWORK" == "sglang-disagg" ]]; then
+    BENCHMARK_SUBDIR="multi_node"
+else
+    BENCHMARK_SUBDIR="single_node"
+fi
+
+set -x
+docker run --rm --init --network host --shm-size=16g --name=$server_name \
 --privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
--e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
--e ISL -e OSL -e PYTHONPYCACHEPREFIX=/tmp/pycache/ -e RANDOM_RANGE_RATIO -e RESULT_FILENAME -e RUN_EVAL -e RUNNER_TYPE \
--e PROFILE -e SGLANG_TORCH_PROFILER_DIR -e VLLM_TORCH_PROFILER_DIR -e VLLM_RPC_TIMEOUT \
+-e HF_TOKEN \
+-e HF_HUB_CACHE \
+-e MODEL \
+-e TP \
+-e CONC \
+-e ISL \
+-e OSL \
+-e MAX_MODEL_LEN \
+-e RANDOM_RANGE_RATIO \
+-e RESULT_FILENAME \
+-e EP_SIZE \
+-e DP_ATTENTION \
+-e RUN_EVAL \
 --entrypoint=/bin/bash \
 $IMAGE \
-benchmarks/single_node/"${EXP_NAME%%_*}_${PRECISION}_mi300x.sh"
+benchmarks/${BENCHMARK_SUBDIR}/${MODEL_CODE}_${PRECISION}_mi350x${FRAMEWORK_SUFFIX}${SPEC_SUFFIX}.sh
+
+if ls gpucore.* 1> /dev/null 2>&1; then
+  echo "gpucore files exist. not good"
+  rm -f gpucore.*
+fi
+
+# for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo $original | sudo tee $cpu; done
+
+# Cleanup: stop server container 
+docker stop $server_name 2>/dev/null || true
+docker rm $server_name 2>/dev/null || true
+
