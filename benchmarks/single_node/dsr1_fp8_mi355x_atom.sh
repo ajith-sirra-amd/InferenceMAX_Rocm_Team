351eb8684c64644d02bdb7f368a1e83b14b591a8
ajith-sirra-amd <ajith.sirra@amd.com>
2026-05-05 10:18:46 +0530

    Updating DeepSeek-R1-0528 Atom

--- benchmarks/single_node/dsr1_fp8_mi355x_atom.sh
+++ benchmarks/single_node/dsr1_fp8_mi355x_atom.sh
@@ -47,8 +47,8 @@ python3 -m atom.entrypoints.openai_server \
     --model $MODEL \
     --server-port $PORT \
     -tp $TP \
-    --kv_cache_dtype fp8 $CALCULATED_MAX_MODEL_LEN $EP \
-    --block-size $BLOCK_SIZE > $SERVER_LOG 2>&1 &
+    --kv_cache_dtype fp8 > $SERVER_LOG 2>&1 &
+    # $CALCULATED_MAX_MODEL_LEN $EP --block-size $BLOCK_SIZE > $SERVER_LOG 2>&1 &
 
 SERVER_PID=$!
 
