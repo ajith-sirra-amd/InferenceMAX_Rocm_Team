dc3db3d2a12f62833e7fb62d937711467e773a32
ajith-sirra-amd <ajith.sirra@amd.com>
2026-06-26 09:37:24 +0530

    DSV4 + LMCACHE - VLLM

--- upstream/InferenceX/benchmarks/single_node/agentic/dsv4_fp4_mi355x.sh
+++ upstream/InferenceX/benchmarks/single_node/agentic/dsv4_fp4_mi355x.sh
@@ -237,7 +237,7 @@ VLLM_CMD=(
     --gpu-memory-utilization 0.8
     --kv-cache-dtype fp8
     --trust-remote-code
-    --moe-backend triton_unfused
+    --moe-backend aiter
     --tokenizer-mode deepseek_v4
     --reasoning-parser deepseek_v4
     --compilation-config '{"mode":3,"cudagraph_mode":"FULL_AND_PIECEWISE"}'
