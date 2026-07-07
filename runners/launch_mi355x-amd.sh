9ee657712a273d26861c4c8901f064a34fb235c9
ajith-sirra-amd <ajith.sirra@amd.com>
2026-07-07 09:11:22 +0530

    MINIMAX-M3 DOCKER 23.0

--- runners/launch_mi355x-amd.sh
+++ runners/launch_mi355x-amd.sh
@@ -2,7 +2,7 @@
 if [[ $RUNNER_NAME == *gbt350* ]]; then
     HF_HUB_CACHE_MOUNT="/data/hf_hub_cache/actions-runner"
 elif [[ $RUNNER_TYPE == "mi355x" ]]; then
-    HF_HUB_CACHE_MOUNT="/raid0/data/" #/it-share/hf_cache/"
+    HF_HUB_CACHE_MOUNT="/data/models" #"/raid0/data/" #/it-share/hf_cache/"
 elif [[ $RUNNER_TYPE == "mi355x-p02-g57" ]]; then
     HF_HUB_CACHE_MOUNT="/mnt/hf_hub_cache/"
 elif [[ $RUNNER_TYPE == "mi350x_ajith_do" ]]; then
