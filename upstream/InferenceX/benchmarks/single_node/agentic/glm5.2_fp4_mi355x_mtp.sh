cff8861777f3a7f5c1bbb48de5c91f1d209094c1
Sirra <asirra@amd.com>
2026-09-10 14:10:04 +0530

    GLM5.2 - Next

--- upstream/InferenceX/benchmarks/single_node/agentic/glm5.2_fp4_mi355x_mtp.sh
+++ upstream/InferenceX/benchmarks/single_node/agentic/glm5.2_fp4_mi355x_mtp.sh
@@ -5,6 +5,7 @@ set -x
 source "$(dirname "$0")/../../benchmark_lib.sh"
 
  export EVAL_FRAMEWORK="lm-eval"
+ export AIPERF_WARMUP_REQUESTS_PER_LANE=1
  
 check_env_vars MODEL TP CONC KV_OFFLOADING TOTAL_CPU_DRAM_GB RESULT_DIR DURATION EP_SIZE DP_ATTENTION
  
